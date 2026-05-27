d7ae8c55adeb526cb04655034fe1d4bcb0e69e73
Keyuan <wangkeyuan1998@gmail.com>
2026-05-27 16:34:41 +0100

    fix: For IntrusiveList, add move constructor/assignment operator, delete copy constructor/assignment operator

--- core/matching_core/include/matching/intrusive_list.hpp
+++ core/matching_core/include/matching/intrusive_list.hpp
@@ -11,6 +11,38 @@ private:
     std::size_t size_ = 0;
 
 public:
+    IntrusiveList() = default;
+
+    // Move constructor
+    IntrusiveList(IntrusiveList&& other) noexcept
+        : head_(other.head_),
+          tail_(other.tail_),
+          size_(other.size_)
+    {
+        other.head_ = nullptr;
+        other.tail_ = nullptr;
+        other.size_ = 0;
+    }
+
+    // Move operator
+    IntrusiveList& operator=(IntrusiveList&& other) noexcept {
+        if (this != &other) {
+            head_ = other.head_;
+            tail_ = other.tail_;
+            size_ = other.size_;
+
+            other.head_ = nullptr;
+            other.tail_ = nullptr;
+            other.size_ = 0;
+        }
+
+        return *this;
+    }
+
+    // Prevent copy constructor (two pointers pointing to the same order pool)
+    IntrusiveList(const IntrusiveList&) = delete;
+    IntrusiveList& operator=(const IntrusiveList&) = delete;
+
     void push_back(Order& o) {
         o.prev = tail_;
         o.next = nullptr;
